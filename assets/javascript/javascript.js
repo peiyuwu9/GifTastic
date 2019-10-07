ba2d4f70d3f70eae8eeafb835e8df46b40e9841b
Kevin Wu <peiyuwu9@gmail.com>
2019-10-07 02:12:12 -0400

    First upload, need to fix favorite gif animation/download gid/list 10 more gifs

---
+++ assets/javascript/javascript.js
@@ -0,0 +1,268 @@
+//Defult some keywords
+var topics = ["natural","animal","music","car"];
+
+//Set a function to render keywords to button
+function renderButtons() {
+
+    // Clean the section before buttons rendered
+    $("#button-display").empty();
+
+    //Loop through the array of keywords
+    for (var i=0; i<topics.length; i++) {
+
+        //Create buttons
+        var button = $("<button>");
+
+        //Give buttons classes and values
+        button.attr("class","btn btn-outline-info keyword-button").attr("button-name",topics[i]);
+
+        //Print texts on button
+        button.text(topics[i]);
+
+        //Add buttons to display section
+        $("#button-display").append(button);
+    }
+}
+
+//Set a gif clear funcion
+function clearGif() {
+    $("#gif-display").empty();
+}
+
+//Generate defult buttons
+renderButtons();
+
+//Set a event listener for users to create button
+$("#add-keyword").on("click", function(event){
+    
+    //Prevent the form from trying to submit without users confirmm
+    event.preventDefault();
+
+    //Push the keywords users input to the keyword array
+    var userInput = $("#keyword-input").val().trim().toLowerCase();
+
+    //Prevent users from generate repeat or empty buttons
+    if (topics.indexOf(userInput) === -1 && $("#keyword-input").val() !== "") {
+        topics.push(userInput);
+
+        //Generate buttons
+        renderButtons();
+    }
+
+    //Clear text field after click generate button
+    $("#keyword-input").val("");  
+    
+    //Once users click button, brower will retrieve gifs and information from Giphy database
+    $(".keyword-button").on("click", function(){
+
+        //Clear gif div
+        clearGif();
+
+        //Grab keyword from button name
+        var keyword = $(this).attr("button-name");
+
+        //Access data base with keywords
+        var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=p9OTXJik3NqMrPTxbEh02Kbk9tBALQNC&limit=10&q=" + keyword;
+
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function(access){
+            
+            //Store retreived data from Giphy in results
+            var results = access.data;
+            
+            //Diaply first 10 results
+            for (j=0; j<results.length; j++) {
+                //Create div for each gif
+                var eachGif = $("<div>");
+                
+                //Create div for title
+                var resultTitle = $("<div>");
+                resultTitle.text([j+1] + ". " + results[j].title);
+
+                //Create div for rating
+                var resultRating = $("<div>");
+                resultRating.text("Rating: " + results[j].rating);
+                
+                //Create img for gif with all gif's url
+                var resultImage = $("<img>");
+                resultImage.attr("src",results[j].images.fixed_height_still.url).attr("image-still",results[j].images.fixed_height_still.url).attr("image-animate",results[j].images.fixed_height.url).attr("state","still").attr("class","gif");
+
+                //Create add to favorite button
+                var addToFavorite = $("<button>");
+                addToFavorite.text("Add to favorite");
+                addToFavorite.attr("class","add-to-favorite");
+                addToFavorite.attr("src",results[j].images.fixed_height_still.url).attr("image-still",results[j].images.fixed_height_still.url).attr("image-animate",results[j].images.fixed_height.url).attr("state","still");
+                
+                //Append all above elements to single div
+                eachGif.append(resultTitle).append(resultRating).append(resultImage).append("<br>").append("<i class='fas fa-star'></i>").append(addToFavorite);
+
+                //Apeend single div to gid display div
+                $("#gif-display").append(eachGif).append("<br>");
+            }
+
+            //Put a horizontal line
+            $("#gif-display").append("<hr class = 'my-4'>");
+
+            //Once users click add to favorite, gifs will populate in favorite div
+            $(".add-to-favorite").on("click",function(){
+                var favoriteURL = $(this).attr("src");
+                var favoriteImageStill = $(this).attr("image-still");
+                var favoriteImageAnimate = $(this).attr("image-animate");
+                var favoriteState = $(this).attr("state");
+                var favoriteClass = $(this).attr("class");
+                var favoriteGif = $("<img>");
+                favoriteGif.attr("src",favoriteURL).attr("image-still",favoriteImageStill).attr("image-animate",favoriteImageAnimate).attr("state",favoriteState).attr("class","favorite-gif");
+
+                $("#favorite-display").append(favoriteGif).append("<br><br>");
+
+                //Once users click on gif, gif will start animating, and click again will stop animation
+                $(".favorite-gif").on("click",function(){
+
+                //Grab image state
+                var currentState = $(this).attr("state");
+    
+                console.log(currentState);
+    
+                //If gif is still, change the src to animated src; vice versa.
+                if (currentState === "still") {
+                    $(this).attr("src",$(this).attr("image-animate"));
+                    $(this).attr("state","animate");
+                }
+                else {
+                    $(this).attr("src",$(this).attr("image-still"));
+                    $(this).attr("state","still");
+                }
+                })
+            })
+
+            //Once users click on gif, gif will start animating, and click again will stop animation
+            $(".gif").on("click",function(){
+
+                //Grab image state
+                var currentState = $(this).attr("state");
+
+                //If gif is still, change the src to animated src; vice versa.
+                if (currentState === "still") {
+                    $(this).attr("src",$(this).attr("image-animate"));
+                    $(this).attr("state","animate");
+                }
+                else {
+                    $(this).attr("src",$(this).attr("image-still"));
+                    $(this).attr("state","still");
+                }
+            })  
+        })
+    })
+})
+
+//Once users click button, brower will retrieve gifs and information from Giphy database
+$(".keyword-button").on("click", function(){
+
+    //Clear gif div
+    clearGif();
+
+    //Grab keyword from button name
+    var keyword = $(this).attr("button-name");
+
+    //Access data base with keywords
+    var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=p9OTXJik3NqMrPTxbEh02Kbk9tBALQNC&limit=10&q=" + keyword;
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function(access){
+        
+        //Store retreived data from Giphy in results
+        var results = access.data;
+        
+        //Diaply first 10 results
+        for (j=0; j<results.length; j++) {
+            //Create div for each gif
+            var eachGif = $("<div>");
+            
+            //Create div for title
+            var resultTitle = $("<div>");
+            resultTitle.text([j+1] + ". " + results[j].title);
+
+            //Create div for rating
+            var resultRating = $("<div>");
+            resultRating.text("Rating: " + results[j].rating);
+            
+            //Create img for gif with all gif's url
+            var resultImage = $("<img>");
+            resultImage.attr("src",results[j].images.fixed_height_still.url).attr("image-still",results[j].images.fixed_height_still.url).attr("image-animate",results[j].images.fixed_height.url).attr("state","still").attr("class","gif");
+
+            //Create add to favorite button
+            var addToFavorite = $("<button>");
+            addToFavorite.text("Add to favorite");
+            addToFavorite.attr("class","add-to-favorite");
+            addToFavorite.attr("src",results[j].images.fixed_height_still.url).attr("image-still",results[j].images.fixed_height_still.url).attr("image-animate",results[j].images.fixed_height.url).attr("state","still");
+
+            //Append all above elements to single div
+            eachGif.append(resultTitle).append(resultRating).append(resultImage).append("<br>").append("<i class='fas fa-star'></i>").append(addToFavorite);
+
+            //Apeend single div to gid display div
+            $("#gif-display").append(eachGif).append("<br>");
+        }
+
+        //Put a horizontal line
+        $("#gif-display").append("<hr class = 'my-4'>");
+
+        //Once users click add to favorite, gifs will populate in favorite div
+        $(".add-to-favorite").on("click",function(){
+            var favoriteURL = $(this).attr("src");
+            var favoriteImageStill = $(this).attr("image-still");
+            var favoriteImageAnimate = $(this).attr("image-animate");
+            var favoriteState = $(this).attr("state");
+            var favoriteGif = $("<img>");
+            favoriteGif.attr("src",favoriteURL).attr("image-still",favoriteImageStill).attr("image-animate",favoriteImageAnimate).attr("state",favoriteState).attr("class","favorite-gif");
+
+            $("#favorite-display").append(favoriteGif).append("<br><br>");
+
+            //Once users click on gif, gif will start animating, and click again will stop animation
+            $(".favorite-gif").on("click",function(){
+
+            //Grab image state
+            var currentState = $(this).attr("state");
+    
+            console.log(currentState);
+    
+            //If gif is still, change the src to animated src; vice versa.
+            if (currentState === "still") {
+                $(this).attr("src",$(this).attr("image-animate"));
+                $(this).attr("state","animate");
+            }
+            else {
+                $(this).attr("src",$(this).attr("image-still"));
+                $(this).attr("state","still");
+            }
+            })
+        })
+
+
+
+        //Once users click on gif, gif will start animating, and click again will stop animation
+        $(".gif").on("click",function(){
+
+            //Grab image state
+            var currentState = $(this).attr("state");
+
+            //If gif is still, change the src to animated src; vice versa.
+            if (currentState === "still") {
+                $(this).attr("src",$(this).attr("image-animate"));
+                $(this).attr("state","animate");
+            }
+            else {
+                $(this).attr("src",$(this).attr("image-still"));
+                $(this).attr("state","still");
+            }
+        })  
+    })
+})
+
+//Clear clear button to clear gif div
+$("#clear").on("click", function(){
+    clearGif();
+})
